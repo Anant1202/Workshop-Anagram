7e90fe822d2b46012a8cf20dc04f324759903769
Anant1202 <anantsingh1202@gmail.com>
2022-06-07 10:36:49 +0530

    Welcome to Anagram Program

---
+++ Anagram/AnagramProgram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class AnagramProgram
+    {
+        public void AP()
+        {
+            Console.WriteLine("Enter string 1");
+            string s1=Console.ReadLine();
+            Console.WriteLine("Enter string 2");
+            string s2 = Console.ReadLine();
+            char[] arr1=s1.ToUpper().ToCharArray();
+            char[] arr2 = s2.ToUpper().ToCharArray();
+            Array.Sort(arr1);
+            Array.Sort(arr2);
+            string s3 = new string(arr1);
+            string s4 = new string(arr2);
+            if(s3==s4)
+            {
+                Console.WriteLine("It is an Anagram");
+            }
+            else
+            {
+                Console.WriteLine("It is not an Anagram");
+            }
+        }
+    }
+}
---
+++ Anagram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Anagram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Anagram Program");
+            AnagramProgram obj = new AnagramProgram();
+            obj.AP();
+        }
+    }
+}
+
